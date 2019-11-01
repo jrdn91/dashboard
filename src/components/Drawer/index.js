43cc81d608063a939f220b6d733e14281d182b65
Jordan Riser <riser.jordan@gmail.com>
2019-11-01 16:47:23 -0400

    cleans up drawer and moves styles to seperate file

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,6 +1,5 @@
 import React from "react"
-import { Hidden, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, IconButton } from "@material-ui/core"
-import { makeStyles, useTheme } from "@material-ui/core/styles"
+import { Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, IconButton } from "@material-ui/core"
 import InboxIcon from "@material-ui/icons/MoveToInbox"
 import MailIcon from "@material-ui/icons/Mail"
 import MenuIcon from "@material-ui/icons/Menu"
@@ -8,73 +7,11 @@ import CalendarIcon from "@material-ui/icons/CalendarToday"
 import FileIcon from "@material-ui/icons/InsertDriveFile"
 import HomeIcon from "@material-ui/icons/Home"
 import classnames from "clsx"
-
-const useStyles = makeStyles(theme => ({
-  menuButtonRoot: {
-    padding: 15
-  },
-  drawer: {
-    width: theme.drawerWidth,
-    transition: theme.transitions.create(["width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  drawerClosed: {
-    width: 54,
-    transition: theme.transitions.create(["width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  drawerList: {
-    width: theme.drawerWidth,
-    backgroundColor: theme.palette.darkGrey,
-    color: "#fff"
-  },
-  listItemRoot: {
-    "&:hover": {
-      backgroundColor: "white",
-      color: theme.palette.text.primary,
-      "& $listIcon": {
-        color: theme.palette.primary.main
-      }
-    }
-  },
-  listIcon: {
-    color: "#fff"
-  },
-  listIconSelected: {
-    color: theme.palette.primary.main
-  },
-  listItemSelected: {
-    backgroundColor: ["#fff","!important"],
-    color: theme.palette.text.primary,
-    "&:after": {
-      width: 4,
-      height: '40%',
-      content: "''",
-      position: 'absolute',
-      top: '50%',
-      backgroundColor: theme.palette.primary.main,
-      transform: 'translateY(-50%)',
-      right: 0,
-    }
-  },
-  toolbar: {
-    ...theme.mixins.toolbar,
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    backgroundColor: theme.palette.primary.dark,
-    color: "#fff"
-  }
-}))
+import styles from "./styles"
 
 
 const DrawerComponent = ({ container, open, handleDrawerToggle }) => {
-  const classes = useStyles()
-  const theme = useTheme();
+  const classes = styles()
   
   const listItemClasses = {
     root: classes.listItemRoot,
---
+++ src/components/Drawer/styles.js
@@ -0,0 +1,63 @@
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+
+export default makeStyles(theme => ({
+  menuButtonRoot: {
+    padding: 15
+  },
+  drawer: {
+    width: theme.drawerWidth,
+    transition: theme.transitions.create(["width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  drawerClosed: {
+    width: 54,
+    transition: theme.transitions.create(["width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerList: {
+    width: theme.drawerWidth,
+    backgroundColor: theme.palette.darkGrey,
+    color: "#fff"
+  },
+  listItemRoot: {
+    "&:hover": {
+      backgroundColor: "white",
+      color: theme.palette.text.primary,
+      "& $listIcon": {
+        color: theme.palette.primary.main
+      }
+    }
+  },
+  listIcon: {
+    color: "#fff"
+  },
+  listIconSelected: {
+    color: theme.palette.primary.main
+  },
+  listItemSelected: {
+    backgroundColor: ["#fff","!important"],
+    color: theme.palette.text.primary,
+    "&:after": {
+      width: 4,
+      height: '40%',
+      content: "''",
+      position: 'absolute',
+      top: '50%',
+      backgroundColor: theme.palette.primary.main,
+      transform: 'translateY(-50%)',
+      right: 0,
+    }
+  },
+  toolbar: {
+    ...theme.mixins.toolbar,
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: theme.palette.primary.dark,
+    color: "#fff"
+  }
+}))
