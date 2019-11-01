e5fae6e9f75671cfb6ace83cd6170011e86bdb21
Jordan Riser <riser.jordan@gmail.com>
2019-11-01 16:11:52 -0400

    adds primary color and overrides

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,27 @@
 import { createMuiTheme } from "@material-ui/core/styles"
 
+const primary = {
+  main: "#30b4e1",
+  dark: "#1ea6d4"
+}
+
 export default createMuiTheme({
-  drawerWidth: 240
+  palette: {
+    primary
+  },
+  drawerWidth: 240,
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          backgroundColor: "#f1f4f8"
+        }
+      }
+    },
+    MuiAppBar: {
+      colorPrimary: {
+        color: "#fff"
+      }
+    }
+  }
 })
