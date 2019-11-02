6d8d814e67a519ba1400d9261a2c89c04cc6eb23
Jordan Riser <riser.jordan@gmail.com>
2019-11-02 13:21:03 -0400

    cleans index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import { MuiThemeProvider } from "@material-ui/core/styles"
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import App from "./components/App"
+import * as serviceWorker from "./serviceWorker"
 import theme from "./theme"
 
 const Application = (
@@ -13,9 +13,9 @@ const Application = (
   </MuiThemeProvider>
 )
 
-ReactDOM.render(Application, document.getElementById('root'));
+ReactDOM.render(Application, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
